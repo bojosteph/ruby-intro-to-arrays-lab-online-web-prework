a5a10e5cb7de14c940d47b45815869789babd993
Roberto Samia <bojosteph@gmail.com>
2018-09-12 00:13:33 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,9 +9,9 @@
    
  end
  
- def first_element
+ def first_element(my_first_element)
    @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-    @taylor_swift[0]
+                  @taylor_swift[0]
  end
  
  def third_element
